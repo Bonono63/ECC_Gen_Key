5634de2fa212d5dd0840b3369df85461ce43a2bd
Bonono63 <bonoria63@gmail.com>
2023-03-02 21:28:48 -0500

    modified:   main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
@@ -11,8 +12,17 @@ struct Point
 
 struct uint256_t
 {
-    uint64_t vec[4];
+    uint64_t d[4];
 };
+
+#define getuint256_t(d7, d6, d5, d4, d3, d2, d1) \
+{{ \
+	(d0) | (((uint64_t)(d1)) << 32),\
+	(d2) | (((uint64_t)(d3)) << 32),\
+	(d4) | (((uint64_t)(d5)) << 32),\
+	(d6) | (((uint64_t)(d7)) << 32)\
+}}/
+
 
 struct Point G = {0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8};
 uint64_t k;
@@ -28,31 +38,59 @@ uint64_t k;
 
 struct Point Q;
 
-void getRandom();
+void getRandom(uint64_t* k);
 
 int main (void)
 {
-    getRandom(&k);
-    printf("%lu\n", k);
+	uint64_t rand[4];
+    	FILE* random;
+
+    	random = fopen("/dev/random", "r");
+    	for(int i = 0; i < 4 ; i++)
+	{
+		uint64_t temp;
+		uint8_t tempa[8];
+		for (int x = 0 ; x < 8 ; x++)
+    		{
+        		uint8_t character;
+        		character = fgetc(random);
+        		printf("%i\n",character);
+			tempa[x] = character;
+		}
+		for (int y = 0; y < 8; ++y)
+    			temp = (temp << 8) | tempa[y];
+
+		rand[i] = temp;
+	}
+
+	printf("%llu\n", rand[0]);
+	printf("%llu\n", rand[1]);
+	printf("%llu\n", rand[2]);
+	printf("%llu\n", rand[3]);
 }
 
+/*
 void getRandom(uint64_t* k)
 {
     FILE* random;
 
-    uint8_t result[4];
+    //uint8_t result[4];
 
     random = fopen("/dev/random", "r");
     for (int i = 0 ; i < 4 ; i++)
     {
+	uint8_t buffer[10];
         uint8_t character;
         character = fgetc(random);
-        printf("%c\n",character);
-        result[i] = character;
+	itoa(character, buffer, 10);
+        printf("%s\n",buffer);
+        //result[i] = character;
     }
 
+    
     memcpy(&result[0], &k, sizeof(uint8_t));
     memcpy(&result[1], &k+0xF, sizeof(uint8_t));
     memcpy(&result[2], &k+0xF+0xF, sizeof(uint8_t));
     memcpy(&result[3], &k+0xF+0xF+0xF, sizeof(uint8_t));
-}
+    
+}*/
